d44bb7ab9ae8f39a4a144f01f8b724b9c32ba210
Milot Mirdita <milot@mirdita.de>
2019-04-04 14:04:24 +0200

    fmemopen issue should now be solved

--- recipes/hhsuite/build.sh
+++ recipes/hhsuite/build.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-fi
-
 mkdir -p build && cd build
 cmake -DCHECK_MPI=0 \
       -DHAVE_MPI=0 \
       -DHAVE_SSE2=1 \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DCMAKE_C_FLAGS=${CFLAGS} \
-      -DCMAKE_CXX_FLAGS=${CXXFLAGS} \
-      ${CMAKE_PLATFORM_FLAGS[@]} \
       ..
 
 make -j${CPU_COUNT} ${VERBOSE_CM}
 make install -j${CPU_COUNT}
-make clean
+
